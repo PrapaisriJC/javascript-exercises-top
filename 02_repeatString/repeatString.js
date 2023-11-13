e5176359c302c2027f5eedc56ba21dbcc8245af7
Caassy <76062390+PrapaisriJC@users.noreply.github.com>
2023-11-14 02:33:35 +0700

    Finish 01 and 02 with all tests pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let finalString = '';
+    if (num < 0){
+        finalString = 'ERROR';
+        return finalString;
+    }
+    for(let i = 0; i < num ; i++){
+        finalString += word;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
