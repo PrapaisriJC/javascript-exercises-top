9353e3e4b01506625d54aa31b048e6a7426fb344
Caassy <76062390+PrapaisriJC@users.noreply.github.com>
2023-11-14 04:13:11 +0700

    Finish 04 and pass all test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...thingToRemove) {
+    const newArray = [];
+    
+    iterateArray:
+    for(const item of array){
+        for(const thing of thingToRemove){
+            if(item === thing){
+                continue iterateArray;
+            }
+        }
+        newArray.push(item);
+    }
+    return newArray;
 };
+
+//dont forget to cemment
+/*** 
+ * Iterate through the first array and check if it is true equal to the thingToremove then skip to the next iteration
+ * else push the value to the new array
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
