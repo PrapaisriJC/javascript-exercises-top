c97754930ffbeec89b31ded22f733c3530b93bdd
Caassy <76062390+PrapaisriJC@users.noreply.github.com>
2023-11-14 05:05:00 +0700

    Finish 07 and pass all test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let newDegree = (degree-32) * (5/9);
+  let result = Math.round(newDegree*10)/10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let newDegree = (degree * (9/5)) + 32;
+  let result = Math.round(newDegree*10)/10;
+  return result;
+  
 };
 
 // Do not edit below this line
