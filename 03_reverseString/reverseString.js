cee7a088e43fe99b0101cc73aa6e9a49ae0c4b4f
Caassy <76062390+PrapaisriJC@users.noreply.github.com>
2023-11-14 03:13:30 +0700

    Finish 03 with some hassle lmao

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,26 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const oldString = word.split('');
+    const newString = [];
+    const wordLength = oldString.length;
+
+    for (i = 0; i < wordLength ; i++){
+        let char = oldString.pop();
+        newString.push(char);
+    }
+
+    let reverseString = newString.join("");
+
+    return reverseString;
 
 };
+
+
+
+//GET word
+//SPLIT word into an array
+//POP each letter in a new array
+//concat each letter in the new array into a new string 
+//RETURN the new string
 
 // Do not edit below this line
 module.exports = reverseString;
