7c5a5f3148d4c68f5e56cf612ee108ee477b9304
Caassy <76062390+PrapaisriJC@users.noreply.github.com>
2023-11-14 04:41:32 +0700

    Finish 05 and pass all test
    I think there's some difference between solution's test and our test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if(num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number"){
+        return 'ERROR';
+    }
+
+    let startNum = (num1 < num2) ? num1 : num2;
+    let endNum = (num1 > num2) ? num1 : num2;
+
+    for(let i = startNum; i <= endNum; i++){
+        sum+=i;
+    }
+    return sum;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
